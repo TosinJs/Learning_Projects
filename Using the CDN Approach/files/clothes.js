33fb7dedb1d8670cdf1a01136f08db8987b79667
Tosin Samuel <TosinJs@users.noreply.github.com>
2021-03-03 00:52:02 +0100

    React Project

---
+++ Using the CDN Approach/files/clothes.js
@@ -0,0 +1,67 @@
+const CLOTHES = [
+    {
+        id: 0,
+        name: 'New nulla pariatur. Anim id est laborum. Jump lex',
+        image: 'assets/four.png',
+        label: 'NEW',
+        price: '$800'
+    },
+    {
+        id: 1,
+        name: 'Gate sunt in culpa qui officia deserunt mollit',
+        image: 'assets/one.png',
+        label: '',
+        price: '$950'
+    },
+    {
+        id: 2,
+        name: 'Excepteur sint occaecat cupidatat non proident',
+        image: 'assets/six.jpg',
+        label: '',
+        price: '$560'
+    },
+    {
+        id: 3,
+        name: 'In voluptate velit esse cillum dolore eu fugiat',
+        image: 'assets/seven.jpg',
+        label: 'NEW',
+        price: '$800'
+    },
+    {
+        id: 4,
+        name: 'Consequat. Duis aute irure dolor in reprehenderit',
+        image: 'assets/five.png',
+        label: 'NEW',
+        price: '$200'
+    },
+    {
+        id: 5,
+        name: 'Exercitation ullamco laboris nisi ut aliquip ex ea commodo',
+        image: 'assets/three.jpg',
+        label: 'NEW',
+        price: '$920'
+    },
+    {
+        id: 6,
+        name: 'Et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud',
+        image: 'assets/eight.jpg',
+        label: 'NEW',
+        price: '$750'
+    },
+    {
+        id: 7,
+        name: 'Sed do eiusmod tempor incididunt ut labore',
+        image: 'assets/two.jpg',
+        label: '',
+        price: '$400'
+    },
+    {
+        id: 8,
+        name: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+        image: 'assets/nine.jpg',
+        label: 'NEW',
+        price: '$600'
+    }
+]
+
+
